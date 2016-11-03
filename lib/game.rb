96594f9e9e51c00f8953b309dad0f8fe6b0e781e
Alexa <alexa.anderson@outlook.com>
2016-11-03 13:23:14 -0500

    Begin pseudotest

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,21 @@
 # - win condition
 # - player turn logic
 
+=begin
+  public interface
+    #new - takes attrs{ player_red, player_blue, board }
+    #play -
+      HP - prints welcome
+         - send message game loop
+    # 
+=end
+
 class Game
   def initialize(attr = {})
-    @playerRed = attr[:playerRed]
-    @playerBlue = attr[:playerBlue]
+    @player_red = attr[:player_red]
+    @player_blue = attr[:player_blue]
     @board = attr[:board]
-    @turnCount = 1 # Odd for red, Even for Blue, at 43 game over
+    @turn_count = 1 # Odd for red, Even for Blue, at 43 game over
     @win = false
   end
 
@@ -19,24 +28,25 @@ class Game
 
   def game_loop
     @board.render
-    drop = current_player(@turnCount).input
-    @board.add_piece(current_player(@turnCount), drop)
+    drop = current_player(@turn_count).get_input
+    @board.add_piece(current_player(@turn_count), drop)
     check_win
   end
 
-  def current_player(count)
-    count % 2 == 0 ? @playerBlue : @playerRed
-  end
+  private
+    def current_player(count)
+      count % 2 == 0 ? @player_blue : @player_red
+    end
 
-  def welcome
-    puts "Welcome to Connect 4!"
-    puts "To place a piece please type the number of the column (0-6)."
-    puts "Press 'enter' to begin."
-    gets
-  end
+    def welcome
+      puts "Welcome to Connect 4!"
+      puts "To place a piece please type the number of the column (0-6)."
+      puts "Press 'enter' to begin."
+      gets
+    end
 
-  def check_win
-    @board.four_in_a_row?(player)
-  end
+    def check_win
+      @board.four_in_a_row?(player)
+    end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,16 @@
-# Player
-# - move (class or method?)
-# - drop (Piece, column)
-# play passes color position of piece to board
+=begin
+  public interface: 
+    color - instance vairable, R
+      - can read
+    symbol - inst var, R
+      - can read
+    #new - takes attrs={color}
+      HP - user initialize with attr
+         - sets color and symbol
+      SP - initialize with nothing
+    #get_input - returns an integer
+      HP - return an integet in column range
+=end
 
 class Player
   attr_reader :color, :symbol
@@ -11,14 +20,9 @@ class Player
     @symbol = self.color == "red" ? 'X' : 'O'
   end
 
-  def input
+  def get_input
     puts "Please type in an column number to drop your piece."
     response = gets.chomp.to_i
     response.match(/\A[0-6]\Z/) ? response : input
   end
-
-  def symbol
-    @symbol
-  end
-
 end
